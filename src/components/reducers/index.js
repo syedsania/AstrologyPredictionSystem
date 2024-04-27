28107a5b7e01da7b14420e056b2de3a8aeb05ea3
syedsania <143679829+syedsania@users.noreply.github.com>
2024-04-27 06:50:09 +0530

    Delete src directory

--- src/components/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import AuthReducer from "./AuthReducer";
-import NavReducer from "./NavReducer";
-import { combineReducers } from "redux";
-
-export default combineReducers({
-    AuthReducer: AuthReducer,
-    NavReducer: NavReducer
-});
